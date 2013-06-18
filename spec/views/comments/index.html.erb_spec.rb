72c2632794393832bf3a2aaff0b334daf13e7bf6
Mayoto Yoshida <2getjp@gmail.com>
2013-06-18 17:26:09 +0900

    scaffold comment して route を変更するまで

---
+++ spec/views/comments/index.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "comments/index" do
+  before(:each) do
+    assign(:comments, [
+      stub_model(Comment,
+        :name => "Name",
+        :content => "Content",
+        :topic => nil
+      ),
+      stub_model(Comment,
+        :name => "Name",
+        :content => "Content",
+        :topic => nil
+      )
+    ])
+  end
+
+  it "renders a list of comments" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Content".to_s, :count => 2
+    assert_select "tr>td", :text => nil.to_s, :count => 2
+  end
+end
---
+++ spec/views/comments/show.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "comments/show" do
+  before(:each) do
+    @comment = assign(:comment, stub_model(Comment,
+      :name => "Name",
+      :content => "Content",
+      :topic => nil
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Name/)
+    rendered.should match(/Content/)
+    rendered.should match(//)
+  end
+end

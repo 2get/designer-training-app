17a3de614e4a78fb7d7c3b89fabe736d70b1a3cc
Mayoto Yoshida <2getjp@gmail.com>
2013-06-18 15:43:02 +0900

    Add validation

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,5 @@
 class Topic < ActiveRecord::Base
+  validates :subject, presence: true, length: { maximum: 140 }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :body, presence: true
 end
